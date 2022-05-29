1924aa81057ce6a5366d2e4f604d625d356e69f1
Raphael Merx <raphael.merx@gmail.com>
2022-05-29 21:08:52 +0800

    Switch to Indonesia VFS application, check if earlier than date

--- vfs_appointment_bot/_VfsClient.py
+++ vfs_appointment_bot/_VfsClient.py
@@ -1,8 +1,7 @@
-from cmath import exp
-import email
 import time
 import logging
-import datetime
+from datetime import datetime
+import re
 
 from _TwilioClient import _TwilioClient
 from _ConfigReader import _ConfigReader
@@ -10,6 +9,7 @@ from _ConfigReader import _ConfigReader
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
+
 
 class _VfsClient:
 
@@ -23,7 +23,7 @@ class _VfsClient:
         # open in headless mode to run in background
         firefox_options.headless = True
         # firefox_options.add_argument("start-maximized")
-        
+
         # following options reduce the RAM usage
         firefox_options.add_argument("disable-infobars")
         firefox_options.add_argument("--disable-extensions")
@@ -32,22 +32,22 @@ class _VfsClient:
         firefox_options.add_argument("--disable-gpu")
         firefox_options.add_argument("--disable-dev-shm-usage")
         self._web_driver = webdriver.Firefox(options=firefox_options)
-        
-        # make sure that the browser is full screen, 
+
+        # make sure that the browser is full screen,
         # else some buttons will not be visible to selenium
         self._web_driver.maximize_window()
 
     def _login(self):
-       
+
         _section_header = "VFS"
-        _email = self._config_reader.read_prop(_section_header, "vfs_email");
-        _password = self._config_reader.read_prop(_section_header, "vfs_password");
-        
+        _email = self._config_reader.read_prop(_section_header, "vfs_email")
+        _password = self._config_reader.read_prop(_section_header, "vfs_password")
+
         logging.debug("Logging in with email: {}".format(_email))
-        
+
         # logging in
-        time.sleep(10)  
-        
+        time.sleep(10)
+
         # sleep provides sufficient time for all the elements to get visible
         _email_input = self._web_driver.find_element_by_xpath("//input[@id='mat-input-0']")
         _email_input.send_keys(_email)
@@ -56,11 +56,11 @@ class _VfsClient:
         _login_button = self._web_driver.find_element_by_xpath("//button/span")
         _login_button.click()
         time.sleep(10)
-        
+
     def _validate_login(self):
         try:
             _new_booking_button = self._web_driver.find_element_by_xpath("//section/div/div[2]/button/span")
-            if _new_booking_button == None:
+            if _new_booking_button is None:
                 logging.debug("Unable to login. VFS website is not responding")
                 raise Exception("Unable to login. VFS website is not responding")
             else:
@@ -70,7 +70,7 @@ class _VfsClient:
             raise Exception("Unable to login. VFS website is not responding")
 
     def _get_appointment_date(self, visa_centre, category, sub_category):
-        logging.info("Getting appointment date: Visa Centre: {}, Category: {}, Sub-Category: {}".format(visa_centre, category, sub_category)) 
+        logging.info("Getting appointment date: Visa Centre: {}, Category: {}, Sub-Category: {}".format(visa_centre, category, sub_category))
         # select from drop down
         _new_booking_button = self._web_driver.find_element_by_xpath(
             "//section/div/div[2]/button/span"
@@ -89,42 +89,42 @@ class _VfsClient:
             )
         except NoSuchElementException:
             raise Exception("Visa centre not found: {}".format(visa_centre))
-        
+
         logging.debug("VFS Centre: " + _visa_centre.text)
         self._web_driver.execute_script("arguments[0].click();", _visa_centre)
         time.sleep(5)
-        
+
         _category_dropdown = self._web_driver.find_element_by_xpath(
             "//div[@id='mat-select-value-3']"
         )
         _category_dropdown.click()
         time.sleep(5)
-        
+
         try:
             _category = self._web_driver.find_element_by_xpath(
                 "//mat-option[starts-with(@id,'mat-option-')]/span[contains(text(), '{}')]".format(category)
             )
         except NoSuchElementException:
             raise Exception("Category not found: {}".format(category))
-        
+
         logging.debug("Category: " + _category.text)
         self._web_driver.execute_script("arguments[0].click();", _category)
         time.sleep(5)
-        
+
         _subcategory_dropdown = self._web_driver.find_element_by_xpath(
             "//div[@id='mat-select-value-5']"
         )
-     
+
         self._web_driver.execute_script("arguments[0].click();", _subcategory_dropdown)
         time.sleep(5)
-        
+
         try:
             _subcategory = self._web_driver.find_element_by_xpath(
                 "//mat-option[starts-with(@id,'mat-option-')]/span[contains(text(), '{}')]".format(sub_category)
             )
         except NoSuchElementException:
             raise Exception("Sub-category not found: {}".format(sub_category))
-        
+
         self._web_driver.execute_script("arguments[0].click();", _subcategory)
         logging.debug("Sub-Cat: " + _subcategory.text)
         time.sleep(5)
@@ -132,11 +132,15 @@ class _VfsClient:
         # read contents of the text box
         return self._web_driver.find_element_by_xpath("//div[4]/div")
 
+    def check_if_earlier(self, message, date):
+        matches = re.match(r'Earliest Available Slot : (\d{2}/\d{2}/\d{4})', message)
+        return datetime.strptime(matches.group(1), "%d/%m/%Y") <= datetime.strptime(date, "%d/%m/%Y")
+
     def check_slot(self, visa_centre, category, sub_category):
         self._init_web_driver()
 
         # open the webpage
-        self._web_driver.get("https://visa.vfsglobal.com/ind/en/deu/login")
+        self._web_driver.get("https://visa.vfsglobal.com/idn/en/nld/login")
 
         self._login()
         self._validate_login()
@@ -146,11 +150,15 @@ class _VfsClient:
 
         if len(_message.text) != 0 and _message.text != "No appointment slots are currently available" and _message.text != "Currently No slots are available for selected category, please confirm waitlist\nTerms and Conditions":
             logging.info("Appointment slots available: {}".format(_message.text))
-            ts = time.time()
-            st = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-            message = "{} at {}".format(_message.text, st)
-            self._twilio_client.send_message(message)
-            self._twilio_client.call()
+            if self.check_if_earlier(_message.text, "01/07/2022"):
+                ts = time.time()
+                st = datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
+                message = "{} at {}".format(_message.text, st)
+                print('message: ', message)
+                # self._twilio_client.send_message(message)
+                # self._twilio_client.call()
+            else:
+                logging.info("No slots available before 01/07/2022")
         else:
             logging.info("No slots available")
         # Close the browser
