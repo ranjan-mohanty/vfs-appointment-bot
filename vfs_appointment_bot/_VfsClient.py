2ef80bbb7fa67b28e9018796aee6650bd02a0680
Mikhail Volkhov <volhovm.cs@gmail.com>
2023-03-06 12:57:24 +0000

    Add multiple countries scanning

--- vfs_appointment_bot/_VfsClient.py
+++ vfs_appointment_bot/_VfsClient.py
@@ -8,9 +8,9 @@ from _ConfigReader import _ConfigReader
 from _TwilioClient import _TwilioClient
 from _TelegramClient import _TelegramClient
 
-
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 class _VfsClient:
@@ -22,10 +22,13 @@ class _VfsClient:
 
         self._use_telegram = self._config_reader.read_prop("DEFAULT", "use_telegram")
         self._use_twilio = self._config_reader.read_prop("DEFAULT", "use_twilio")
-        logging.debug("Will use Telegram : {}".format(self._use_telegram))
-        logging.debug("Will use Twilio : {}".format(self._use_twilio))
+        logging.info("Will use Telegram : {}".format(self._use_telegram))
+        logging.info("Will use Twilio : {}".format(self._use_twilio))
+
+        self._init_web_driver()
 
     def _init_web_driver(self):
+        logging.info("Initialising the browser")
         firefox_options = Options()
 
         # open in headless mode to run in background
@@ -39,11 +42,13 @@ class _VfsClient:
         firefox_options.add_argument("--disable-application-cache")
         firefox_options.add_argument("--disable-gpu")
         firefox_options.add_argument("--disable-dev-shm-usage")
+
         self._web_driver = webdriver.Firefox(options=firefox_options)
 
         # make sure that the browser is full screen,
         # else some buttons will not be visible to selenium
         self._web_driver.maximize_window()
+        logging.info("Browser initialised")
 
     def _login(self):
 
@@ -51,75 +56,101 @@ class _VfsClient:
         _email = self._config_reader.read_prop(_section_header, "vfs_email");
         _password = self._config_reader.read_prop(_section_header, "vfs_password");
 
-        logging.debug("Logging in with email: {}".format(_email))
-
+        logging.info("Loading the login page")
         # logging in
-        time.sleep(10)
+        time.sleep(30)
+        logging.info("Logging in with email: {}".format(_email))
 
-        # sleep provides sufficient time for all the elements to get visible
-        _email_input = self._web_driver.find_element_by_xpath("//input[@id='mat-input-0']")
-        _email_input.send_keys(_email)
-        _password_input = self._web_driver.find_element_by_xpath("//input[@id='mat-input-1']")
-        _password_input.send_keys(_password)
-        _login_button = self._web_driver.find_element_by_xpath("//button/span")
-        _login_button.click()
-        time.sleep(10)
+        try:
+            # sleep provides sufficient time for all the elements to get visible
+            logging.info("Choosing email input")
+            _email_input = self._web_driver.find_element(By.ID, "mat-input-0")
+            _email_input.send_keys(_email)
+            logging.info("Choosing password input")
+            _password_input = self._web_driver.find_element(By.ID, "mat-input-1")
+            _password_input.send_keys(_password)
+            logging.info("Choosing login button")
+            _login_button = self._web_driver.find_element(By.XPATH, '//button/span[1]')
+            _login_button.click()
+        except Exception as e:
+            logging.info("Couldn't fill out login form")
+            logging.info(e.args[0])
+            #logging.info(self._web_driver.page_source)
+            if "You do not have access" in self._web_driver.page_source:
+                logging.info("Request is banned by CloudFlare")
+            raise e
+
+        logging.info("Requested login, waiting to log in")
+        time.sleep(30)
 
     def _validate_login(self):
         try:
-            _new_booking_button = self._web_driver.find_element_by_xpath("//section/div/div[2]/button/span")
+#            _new_booking_button = self._web_driver.find_element_by_xpath("//section/div/div[2]/button/span")
+            _new_booking_button = self._web_driver.find_element(By.XPATH, '/html/body/app-root/div/app-dashboard/section[1]/div/div[2]/button/span[1]')
             if _new_booking_button == None:
-                logging.debug("Unable to login. VFS website is not responding")
+                logging.info("Unable to login. VFS website is not responding")
+                #logging.info(self._web_driver.page_source)
                 raise Exception("Unable to login. VFS website is not responding")
             else:
-                logging.debug("Logged in successfully")
+                logging.info("Logged in successfully")
         except:
-            logging.debug("Unable to login. VFS website is not responding")
+            logging.info("Unable to login. VFS website is not responding")
+            #logging.info(self._web_driver.page_source)
             raise Exception("Unable to login. VFS website is not responding")
 
-    def _get_appointment_date(self, visa_centre, category, sub_category):
-        logging.info("Getting appointment date: Visa Centre: {}, Category: {}, Sub-Category: {}".format(visa_centre, category, sub_category))
-        # select from drop down
-        _new_booking_button = self._web_driver.find_element_by_xpath(
+        time.sleep(5)
+        _new_booking_button = self._web_driver.find_element(By.XPATH,
             "//section/div/div[2]/button/span"
         )
         _new_booking_button.click()
+
+
+    def _get_appointment_date(self, centre_param):
+        visa_centre = centre_param[0]
+        category = centre_param[1]
+        sub_category = centre_param[2]
+
+        logging.info("Getting appointment date: Visa Centre: {}, Category: {}, Sub-Category: {}".format(visa_centre, category, sub_category))
+
         time.sleep(5)
-        _visa_centre_dropdown = self._web_driver.find_element_by_xpath(
+        #logging.info("Choosing visa centre")
+        _visa_centre_dropdown = self._web_driver.find_element(By.XPATH,
             "//mat-form-field/div/div/div[3]"
         )
         _visa_centre_dropdown.click()
-        time.sleep(2)
+        time.sleep(5)
 
+        #logging.info("Trying to choose visa centre")
         try:
-            _visa_centre = self._web_driver.find_element_by_xpath(
+            _visa_centre = self._web_driver.find_element(By.XPATH,
                 "//mat-option[starts-with(@id,'mat-option-')]/span[contains(text(), '{}')]".format(visa_centre)
             )
         except NoSuchElementException:
             raise Exception("Visa centre not found: {}".format(visa_centre))
 
-        logging.debug("VFS Centre: " + _visa_centre.text)
+        logging.info("VFS Centre: " + _visa_centre.text)
         self._web_driver.execute_script("arguments[0].click();", _visa_centre)
         time.sleep(5)
 
-        _category_dropdown = self._web_driver.find_element_by_xpath(
+        #logging.info("Choosing category")
+        _category_dropdown = self._web_driver.find_element(By.XPATH,
             "//div[@id='mat-select-value-3']"
         )
         _category_dropdown.click()
         time.sleep(5)
 
         try:
-            _category = self._web_driver.find_element_by_xpath(
+            _category = self._web_driver.find_element(By.XPATH,
                 "//mat-option[starts-with(@id,'mat-option-')]/span[contains(text(), '{}')]".format(category)
             )
         except NoSuchElementException:
             raise Exception("Category not found: {}".format(category))
 
-        logging.debug("Category: " + _category.text)
+        logging.info("Category: " + _category.text)
         self._web_driver.execute_script("arguments[0].click();", _category)
         time.sleep(5)
 
-        _subcategory_dropdown = self._web_driver.find_element_by_xpath(
+        _subcategory_dropdown = self._web_driver.find_element(By.XPATH,
             "//div[@id='mat-select-value-5']"
         )
 
@@ -127,44 +158,56 @@ class _VfsClient:
         time.sleep(5)
 
         try:
-            _subcategory = self._web_driver.find_element_by_xpath(
+            _subcategory = self._web_driver.find_element(By.XPATH,
                 "//mat-option[starts-with(@id,'mat-option-')]/span[contains(text(), '{}')]".format(sub_category)
             )
         except NoSuchElementException:
             raise Exception("Sub-category not found: {}".format(sub_category))
 
         self._web_driver.execute_script("arguments[0].click();", _subcategory)
-        logging.debug("Sub-Cat: " + _subcategory.text)
-        time.sleep(5)
+        logging.info("Sub-Cat: " + _subcategory.text)
+        #logging.info("Awaiting response")
+        time.sleep(10)
 
         # read contents of the text box
-        return self._web_driver.find_element_by_xpath("//div[4]/div")
+        contents = self._web_driver.find_element(By.XPATH,"//div[4]/div")
+        logging.info("Found response")
+        return contents
 
-    def check_slot(self, visa_centre, category, sub_category):
-        self._init_web_driver()
+    def check_slot(self, country, vfs_url, centre_params):
 
         # open the webpage
-        self.vfs_login_url = self._config_reader.read_prop("VFS", "vfs_login_url")
+        self.vfs_login_url = vfs_url
+        self._web_driver.delete_all_cookies()
         self._web_driver.get(self.vfs_login_url)
 
         self._login()
         self._validate_login()
 
-        _message = self._get_appointment_date(visa_centre, category, sub_category)
-        logging.debug("Message: " + _message.text)
+        for centre_param in centre_params:
+            logging.info("Checking for params: {}".format(centre_param))
 
-        if len(_message.text) != 0 and _message.text != "No appointment slots are currently available" and _message.text != "Currently No slots are available for selected category, please confirm waitlist\nTerms and Conditions":
-            logging.info("Appointment slots available: {}".format(_message.text))
-            ts = time.time()
-            st = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-            message = "{} at {}".format(_message.text, st)
-            if eval(self._use_telegram):
-                self._telegram_client.send_message(message)
-            if eval(self._use_twilio):
-                self._twilio_client.send_message(message)
-                self._twilio_client.call()
-        else:
-            logging.info("No slots available")
-        # Close the browser
-        self._web_driver.close()
+            _message = self._get_appointment_date(centre_param)
+            logging.info("Message: " + _message.text)
+
+            if len(_message.text) != 0 and "No appointment slots" not in _message.text:
+                for i in range(5):
+                    logging.info("======================================================")
+                logging.info("Appointment slots available: {}".format(_message.text))
+                ts = time.time()
+                st = datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M")
+                message = "{}: {}, {}, msg: {}".format(st, country, centre_param[2], _message.text)
+                if eval(self._use_telegram):
+                    self._telegram_client.send_message(message)
+                if eval(self._use_twilio):
+                    self._twilio_client.send_message(message)
+                    self._twilio_client.call()
+            else:
+                logging.info("No slots available")
+
+        # Otherwise some inactivity issues happen sometimes.
+        self._web_driver.get("https://google.com")
+
+
+    def close(self):
         self._web_driver.quit()
--- vfs_appointment_bot/vfs_appointment_bot.py
+++ vfs_appointment_bot/vfs_appointment_bot.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import signal
 
 from logging.config import fileConfig
 from _Timer import countdown
@@ -7,18 +8,49 @@ from _ConfigReader import _ConfigReader
 from _VfsClient import _VfsClient
 
 def _input():
-    print("Enter the visa centre: ")
-    visa_centre = input()
+    centres = {
+        "Portugal":
+        ("https://visa.vfsglobal.com/gbr/en/prt/login",
+         [ ("Portugal Visa application centre Edinburgh", "GENERAL", "Business Visa"),
+          ("Portugal Visa application centre Edinburgh", "GENERAL", "Tourist visa")
+         ]),
+        "Netherlands":
+        ("https://visa.vfsglobal.com/gbr/en/nld/login",
+         [("Netherlands Visa application center- Edinburgh", "Default Netherlands United Kingdom", "Tourism")]),
+        "Slovenia" :
+        ("https://visa.vfsglobal.com/gbr/en/svn/login",
+         [("Slovenia Visa Application Center - Edinburgh", "Visa", "All Visa Categories")]),
+        "Italy" :
+        ("https://visa.vfsglobal.com/gbr/en/ita/login",
+         [("Italy Visa Application Centre Edinburgh", "Italy UK VisaCategory", "Tourist")]),
+        "Norway" :
+        ("https://visa.vfsglobal.com/gbr/en/nor/login",
+         [("Norway Visa Application Centre, Edinburgh", "Default_Norway_United Kingdom", "Tourist Visa")]),
+        "Malta":
+        ("https://visa.vfsglobal.com/gbr/en/mlt/login",
+         [("Malta Visa application center- Edinburgh", "Short Stay Schengen Visa", "Short Stay Visa")]),
+    }
 
-    print("Enter the category: ")
-    category = input()
+#    print("Enter the visa centre: ")
+#    #visa_centre = input()
+#    visa_centre = "Portugal Visa application centre Edinburgh"
+#
+#    print("Enter the category: ")
+#    #category = input()
+#    category = "GENERAL"
+#
+#    print("Enter the sub category: ")
+#    sub_category = "Tourist visa"
+#    #sub_category = input()
 
-    print("Enter the sub category: ")
-    sub_category = input()
+    logging.info("Processing countries: ")
+    for key in centres:
+        logging.info("  {}, URL: {}".format(key, centres[key][0]))
+        for r in centres[key][1]:
+            logging.info("    Centre: {}, Category: {}, Sub-Category: {}".format(r[0],r[1],r[2]))
+        logging.info("")
 
-    logging.debug("Visa centre: {}, Category: {}, Sub-Category: {}".format(visa_centre, category, sub_category))
-
-    return visa_centre, category, sub_category
+    return centres
 
 def _read_command_line_args():
     if len(sys.argv) != 4:
@@ -32,23 +64,38 @@ if __name__ == "__main__":
     logging = logging.getLogger(__name__);
 
     _vfs_client = _VfsClient()
+
+    def handler(signum, frame):
+        logging.info("Ctrl-c was pressed. Closing browser")
+        try:
+            _vfs_client.close()
+            logging.info("Browser closed")
+        except Exception as e:
+            logging.info("Couldn't close the browser")
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, handler)
+
     _config_reader = _ConfigReader()
     _interval = _config_reader.read_prop("DEFAULT", "interval")
     logging.debug("Interval: {}".format(_interval))
 
-    visa_centre, category, sub_category = _read_command_line_args()
+    centres = _read_command_line_args()
+    #visa_centre, category, sub_category = _read_command_line_args()
 
     logging.info("Starting VFS Appointment Bot")
     while True:
-        try:
-            logging.info("Running VFS Appointment Bot: Attempt#{}".format(count))
-            _vfs_client.check_slot(visa_centre=visa_centre, category=category, sub_category=sub_category)
-            logging.debug("Sleeping for {} seconds".format(_interval))
-            countdown(int(_interval))
-        except Exception as e:
-            logging.info(e.args[0] + ". Please check the logs for more details")
-            logging.debug(e, exc_info=True, stack_info=True)
-            countdown(int(60))
-            pass
-        print("\n")
-        count += 1
+        for country in centres:
+            logging.info("Running, country {}, iteration#{}".format(country,count))
+            try:
+                _vfs_client.check_slot(country,centres[country][0],centres[country][1])
+                logging.debug("Sleeping for {} seconds".format(_interval))
+                countdown(int(_interval))
+            except Exception as e:
+                logging.info(e.args[0] + ". Please check the logs for more details")
+                logging.debug(e, exc_info=True, stack_info=True)
+                countdown(int(_interval))
+                pass
+            print("\n")
+            count += 1
